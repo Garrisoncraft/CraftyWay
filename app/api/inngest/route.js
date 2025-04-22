41ff80ce596a24dc54a4491363d5a90a988e161e
Garrisoncraft <garrisonsayor@gmail.com>
2025-04-22 08:49:04 -0700

    Completed Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,11 @@
+import { syncUserCreation, syncUserUpdate } from "@/config/inngest";
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "../../../config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-     syncUserCreation,
-     syncUserUpdate, 
-     syncUserDeletion
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserCreation
   ], 
 });
